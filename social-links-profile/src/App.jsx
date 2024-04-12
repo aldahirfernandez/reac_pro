9abd3f02849bdf94e95a6d44647a3054f087b18c
Aldahir Fernandez <aldahirfernandez27@gmail.com>
2024-04-11 23:06:17 -0400

    Estilo mejorado

--- social-links-profile/src/App.jsx
+++ social-links-profile/src/App.jsx
@@ -1,30 +1,11 @@
-import "./App.css";
-import avatarJessica from "../public/avatar-jessica.jpeg"
+import React from "react";
+import Card from "./components/Card";
 
 function App() {
   return (
-    <>
-      <div className="card">
-        <div className="contenedor">
-          
-          <img src={avatarJessica} alt="imagen de mujer"/>
-
-          <h1>Jessica Randall</h1>
-
-          <h2>London, United Kingdom</h2>
-
-          <p>"Front-end developer and avid reader".</p>
-        </div>
-
-        <div className="buttons">
-          <button className="botones">GitHub</button>
-          <button className="botones">Frontend Mentor</button>
-          <button className="botones">LinkedIn</button>
-          <button className="botones">Twitter</button>
-          <button className="botones">Instagram</button>
-        </div>
-      </div>
-    </>
+    <div>
+      <Card />
+    </div>
   );
 }
 
