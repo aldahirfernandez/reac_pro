3e7527401f8bb41f109b0a961badba62d09b5242
Aldahir Fernandez <aldahirfernandez27@gmail.com>
2024-04-17 22:32:39 -0400

    Rick and Morty

--- social-links-profile/src/App.jsx
+++ social-links-profile/src/App.jsx
@@ -1,24 +1,33 @@
 import React, { useEffect, useState } from "react";
 import Card from "./components/Card.jsx";
 
-function App() {
-  const [users, setUsers] = useState([]);
+function NumerosAleatorios() {
+  const numeros = [];
+  for (let i = 0; i < 10; i++) {
+    numeros.push(Math.floor(Math.random() * 857)); 
+  }
+  numeros.toString();
+  return numeros;
+}
 
+function App() {
+  const [characters, setcharacters] = useState([])
   useEffect(() => {
-    fetch("https://661038830640280f219c9b3d.mockapi.io/api/v2/users")
+    fetch("https://rickandmortyapi.com/api/character/" + NumerosAleatorios())
       .then((res) => {
         return res.json();
       })
       .then((data) => {
-        setUsers(data);
-        //console.log(data);
+        setcharacters(data)
+        console.log(data)
       });
-  }, []);
+  
+  }, [])
 
   return (
     <div>
-      {users.map((user) => (
-        <Card key={user.id} user={user} />
+      {characters.map((character) => (
+        <Card key={character.id} character={character} />
       ))}
     </div>
   );
